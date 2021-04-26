24f3eff85650d9f11ab22d3ffa40e0f46209dfe0
Sebastiandg7 <sebastiandg7@gmail.com>
2021-04-26 16:15:44 -0500

    update JSON

--- src/package/components/cards/cards_types/projects/utils/images.js
+++ src/package/components/cards/cards_types/projects/utils/images.js
@@ -1,1 +1,2 @@
-export const DEFAULT_PROJECT_IMAGE = 'https://cdn.filestackcontent.com/resize=w:500/compress/dLXE4WC8S3mZFZNfClTK';
+export const DEFAULT_PROJECT_IMAGE =
+    'https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1216&q=80';
